c1187d27604bc256b87faa317c42d17bd68648c9
marouabn <marwa.bnsalah98@gmail.com>
2023-01-14 22:17:34 +0100

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDKkgavCqzz2xjX36i278RLGEKnn_RLTck',
+    authDomain: 'projet-11018.firebaseapp.com',
+    projectId: 'projet-11018',
+    storageBucket: 'projet-11018.appspot.com',
+    messagingSenderId: '71725267714',
+    appId: '1:71725267714:web:bb98e3b9c49647a8a5601b'
+  }
+};
+
